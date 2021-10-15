93cbf3994f1b749a41f55926de6d8242b6bdc2b2
Joe O'Bryant <obryantj2383@uhcl.edu>
2021-10-15 14:25:28 -0500

    Added interactivity and hexadecimal conversion.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include "ieee754.h"
+#include <string>
 
 using namespace std;
 
@@ -11,46 +12,114 @@ using namespace std;
 // Main Program
 int main()
 {
+    char activity;
+    char numberFormat;
+    bool activityValid;
+    bool numberFormatValid;
+    string numberFormatString;
+    string numberFromUser;
+    unsigned int *ieee;
+    string binStr;
+    string hexStr;
+ 
 
     // Instantiate the union
-    myfloat var;
+    myfloat var{};
 
     // Get the real value
-    var.f = -2.25;
+    //var.f = -2.25;
 
     // Get the IEEE floating point representation
-    cout << "IEEE 754 representation of ";
-    cout << fixed << setprecision(6) << var.f << " is : " << endl;
-    printIEEE(var);
+    cout << "This application will allow you to several different operations with floating point numbers.\n";
+    cout << "Which activity would you like to do?\n(T)ransform a floating point number from one format to another.\n(C)alculate the value of two numbers and show their IEEE 754 binary, decimal, and hexadecimal formats.\n";
+    cout << "Your choice: ";
+    cin >> activity;
+    activityValid = activity == 't' || activity == 'T' || activity == 'c' || activity == 'C';
+    while (!activityValid) {
+        cout << activity << " is not available as an option for this step. Please type either t, T, c, or C.\n";
+        cout << "Your choice: ";
+        cin >> activity;
+        activityValid = activity == 't' || activity == 'T' || activity == 'c' || activity == 'C';
+    }
+    if (activity == 'T' || activity == 't') {
+        cout << "\n\nFORMAT TRANSFORMER";
+        cout << "Is the number you're providing (h)exadecimal, (d)ecimal, or (b)inary?\n";
+        cout << "Your choice: ";
+        cin >> numberFormat;
+        numberFormatValid = numberFormat == 'h' || numberFormat == 'H' || numberFormat == 'd' || numberFormat == 'D' || numberFormat == 'b' || numberFormat == 'B';
+        while (!numberFormatValid) {
+            cout << numberFormat << " is not available as an option for this step. Please type either h, H, d, D, b, or B.\n";
+            cout << "Your choice: ";
+            cin >> activity;
+            numberFormatValid = numberFormat == 'h' || numberFormat == 'H' || numberFormat == 'd' || numberFormat == 'D' || numberFormat == 'b' || numberFormat == 'B';
+        }
+        if (numberFormat == 'h' || numberFormat == 'H') {
+            cout << "Please type the number in hexadecimal format: ";
+            cin >> numberFromUser;
+            hexStr = numberFromUser;
+            binStr = convertHexStrToBinStr(numberFromUser);
+            ieee = convertStrToIntArr(binStr);
+            // Convert the least significant
+            // mantissa part (23 bits)
+            // to corresponding decimal integer
+            unsigned int f = convertToInt(ieee, 9, 31);
 
-    // Get the 32-bit floating point number
-    unsigned int ieee[32]
-        = { 1,
-            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Assign integer representation of mantissa
+            var.raw.mantissa = f;
 
-    // Convert the least significant
-    // mantissa part (23 bits)
-    // to corresponding decimal integer
-    unsigned int f = convertToInt(ieee, 9, 31);
+            // Convert the exponent part (8 bits)
+            // to a corresponding decimal integer
+            f = convertToInt(ieee, 1, 8);
 
-    // Assign integer representation of mantissa
-    var.raw.mantissa = f;
+            // Assign integer representation
+            // of the exponent
+            var.raw.exponent = f;
 
-    // Convert the exponent part (8 bits)
-    // to a corresponding decimal integer
-    f = convertToInt(ieee, 1, 8);
+            // Assign sign bit
+            var.raw.sign = ieee[0];
+        }
+        else if (numberFormat == 'd' || numberFormat == 'D') {
+            cout << "Please type the number in decimal format: ";
+            cin >> numberFromUser;
+            var.f = std::stof(numberFromUser);
+            binStr = to_string(var.raw.sign) + returnBinary(var.raw.exponent, 8) + returnBinary(var.raw.mantissa, 23);
+            cout << "\n";
+        }
+        else {
+            cout << "Please type the number in binary format: ";
+            cin >> numberFromUser;
+            ieee = convertStrToIntArr(numberFromUser);
+            binStr = numberFromUser;
+            // Convert the least significant
+            // mantissa part (23 bits)
+            // to corresponding decimal integer
+            unsigned int f = convertToInt(ieee, 9, 31);
 
-    // Assign integer representation
-    // of the exponent
-    var.raw.exponent = f;
+            // Assign integer representation of mantissa
+            var.raw.mantissa = f;
 
-    // Assign sign bit
-    var.raw.sign = ieee[0];
+            // Convert the exponent part (8 bits)
+            // to a corresponding decimal integer
+            f = convertToInt(ieee, 1, 8);
 
-    cout << "The float value of the given"
-        " IEEE-754 representation is : \n";
-    cout << fixed << setprecision(6) << var.f << endl;
-    return 0;
+            // Assign integer representation
+            // of the exponent
+            var.raw.exponent = f;
+
+            // Assign sign bit
+            var.raw.sign = ieee[0];
+        }
+
+        cout << "IEEE 754 representation: ";
+        printIEEE(var);
+        cout << "Decimal representation: ";
+        cout << fixed << setprecision(6) << var.f << endl;
+        cout << "Hexadecimal representation: ";
+        cout << convertBinStrToHexStr(binStr);
+
+        return 0;
+    }
+    else {
+
+    }
 }
--- ConsoleApplication1/ieee754.h
+++ ConsoleApplication1/ieee754.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <iostream>
 #include <iomanip>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -40,6 +44,24 @@ void printBinary(int n, int i)
     }
 }
 
+string returnBinary(int n, int i)
+{
+
+	// Returns the binary representation
+	// of a number n up to i-bits.
+
+	int k;
+	string bin = "";
+	for (k = i - 1; k >= 0; k--) {
+
+		if ((n >> k) & 1)
+			bin = bin + "1";
+		else
+			bin = bin + "0";
+	}
+	return bin;
+}
+
 // This function converts a real value
 // to its IEEE floating point representation.
 void printIEEE(myfloat var)
@@ -64,4 +86,177 @@ unsigned int convertToInt(unsigned int* arr, int low, int high)
         f = f + arr[i] * pow(2, high - i);
     }
     return f;
+}
+
+unsigned int* convertStrToIntArr(string str)
+{
+    static unsigned int arr[32];
+    int i = 0;
+    for (i = 0; i < 32; i++) {
+        arr[i] = str[i] - '0';
+    }
+    return arr;
+}
+
+string convertBinStrToHexStr(string bin)
+{
+	string rest("0x"), tmp, chr = "0000";
+	int len = bin.length() / 4;
+	chr = chr.substr(0, len);
+	bin = chr + bin;
+	for (int i = 0; i < bin.length(); i += 4)
+	{
+		tmp = bin.substr(i, 4);
+		if (!tmp.compare("0000"))
+		{
+			rest = rest + "0";
+		}
+		else if (!tmp.compare("0001"))
+		{
+			rest = rest + "1";
+		}
+		else if (!tmp.compare("0010"))
+		{
+			rest = rest + "2";
+		}
+		else if (!tmp.compare("0011"))
+		{
+			rest = rest + "3";
+		}
+		else if (!tmp.compare("0100"))
+		{
+			rest = rest + "4";
+		}
+		else if (!tmp.compare("0101"))
+		{
+			rest = rest + "5";
+		}
+		else if (!tmp.compare("0110"))
+		{
+			rest = rest + "6";
+		}
+		else if (!tmp.compare("0111"))
+		{
+			rest = rest + "7";
+		}
+		else if (!tmp.compare("1000"))
+		{
+			rest = rest + "8";
+		}
+		else if (!tmp.compare("1001"))
+		{
+			rest = rest + "9";
+		}
+		else if (!tmp.compare("1010"))
+		{
+			rest = rest + "A";
+		}
+		else if (!tmp.compare("1011"))
+		{
+			rest = rest + "B";
+		}
+		else if (!tmp.compare("1100"))
+		{
+			rest = rest + "C";
+		}
+		else if (!tmp.compare("1101"))
+		{
+			rest = rest + "D";
+		}
+		else if (!tmp.compare("1110"))
+		{
+			rest = rest + "E";
+		}
+		else if (!tmp.compare("1111"))
+		{
+			rest = rest + "F";
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return rest;
+}
+
+string convertHexStrToBinStr(string hex)
+{
+	string rest, tmp, chr = "0000";
+	//int len = hex.length() * 4;
+	//chr = chr.substr(0, len);
+	//hex = chr + bin;
+	for (int i = 0; i < hex.length(); i++)
+	{
+		tmp = hex.substr(i, 1);
+		transform(tmp.begin(), tmp.end(), tmp.begin(), ::toupper);
+		if (!tmp.compare("0"))
+		{
+			rest = rest + "0000";
+		}
+		else if (!tmp.compare("1"))
+		{
+			rest = rest + "0001";
+		}
+		else if (!tmp.compare("2"))
+		{
+			rest = rest + "0010";
+		}
+		else if (!tmp.compare("3"))
+		{
+			rest = rest + "0011";
+		}
+		else if (!tmp.compare("4"))
+		{
+			rest = rest + "0100";
+		}
+		else if (!tmp.compare("5"))
+		{
+			rest = rest + "0101";
+		}
+		else if (!tmp.compare("6"))
+		{
+			rest = rest + "0110";
+		}
+		else if (!tmp.compare("7"))
+		{
+			rest = rest + "0111";
+		}
+		else if (!tmp.compare("8"))
+		{
+			rest = rest + "1000";
+		}
+		else if (!tmp.compare("9"))
+		{
+			rest = rest + "1001";
+		}
+		else if (!tmp.compare("A"))
+		{
+			rest = rest + "1010";
+		}
+		else if (!tmp.compare("B"))
+		{
+			rest = rest + "1011";
+		}
+		else if (!tmp.compare("C"))
+		{
+			rest = rest + "1100";
+		}
+		else if (!tmp.compare("D"))
+		{
+			rest = rest + "1101";
+		}
+		else if (!tmp.compare("E"))
+		{
+			rest = rest + "1110";
+		}
+		else if (!tmp.compare("F"))
+		{
+			rest = rest + "1111";
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return rest;
 }
